3d063b463c0077a497a0847d9facbdca5053cc62
Gregoire HENRY <gregoire.henry@ipsa.fr>
2020-01-23 14:52:04 +0100

    obliquity report

--- code/HOURANGLE.m
+++ code/HOURANGLE.m
@@ -9,7 +9,8 @@ S0         = 1361.5;
 SfBoltz    = 5.670374419e-8;
 
 % SETTINGS
-p     = 20*60;
+p     = 11.92*3600;
+porb  = 11.92*3600;
 gamma = 500;
 rho   = 2146;
 cp    = 600;
@@ -18,18 +19,18 @@ emi   = 0.9;
 C     = 0.25;
 dt    = 30;
 t0    = 0;
-tf    = 10*day;
-tfr   = 5*60;
+tf    = 50*p;
+tfr   = 0.25*p;
 tf    = tf+tfr;
 
 % COMPUTE POSITION
-r = [0; 1; 0]*au; 
+r = -[0; 1.0748; 0]*au; 
 dau = my_norm(r)/au;
 
 % ASTEROID GROUND PROPERTIES COMPUTED
 k     = gamma^2/(rho*cp);
 alpha = k/(rho*cp);
-ls    = sqrt(alpha*pi*p);
+ls    = sqrt(alpha*pi*porb);
 V     = C*(rho*cp/gamma)^2;
 c1    = S0*A;
 c2    = S0*(1-A);
@@ -37,11 +38,11 @@ c3    = emi*SfBoltz;
 nls   = 1;
 
 % ASTEROID SHAPE MODEL
-objpath='HO3.obj';
+objpath='didy.obj';
 %objpath = '../kernels/dsk/hera_didymoon_k001_v01.obj';
 obj = read_obj(objpath);
 tris = set_tris(obj);
-m = tris.m'*1e3;
+m = tris.m';
 n = tris.n';
 sph = tris.sph;
 
@@ -54,12 +55,13 @@ n = n(:,condlo);
 np = length(n);
 
 % OBLIQUITY MODEL
-obl   = 0;
+obl   = 162;
 rotv0 = [1; 0; 0];
 rotv  = [0; -sind(obl); cosd(obl)];
-mrot  = mrotv3(rotv, dt*2*pi/p);
-m     = mrotv3(rotv0, obl*pi/180)*m;
-n     = mrotv3(rotv0, obl*pi/180)*n;
+mrot0 = mrotv3(rotv0, obl*pi/180);
+mrot  = mrotv3(rotv,  dt*2*pi/p);
+m     = mrot0*m;
+n     = mrot0*n;
 
 % NUMERICAL MODEL DISCRETIZATION
 et   = t0:dt:tf;
@@ -108,16 +110,23 @@ la = sph(:,2)*180/pi;
 hr = linspace(0,359,ntp);
 [XX, YY, ZZ] = supercontour(hr, la, u);
 close all;
-hold on; grid on; axis equal;
+hold on; grid on; %axis equal;
 xlim([min(min(XX)) max(max(XX))]);
 ylim([min(min(YY)) max(max(YY))]);
 yticks(-80:20:80);
-level = 0:10:400;
+level = 0:15:400;
 xlabel('Hour angle [deg]'); ylabel('Latitude [deg]');
-[C,h] = contourf(XX,YY,ZZ,level,'showtext','on','linestyle','-');
-colormap jet; caxis([0 400]);
-clabel(C,h,'FontSize',8,'FontSmoothing','on');
-title(sprintf('%.4f AU - %s=%d', dau, '\Gamma', gamma));
+[C,h] = contourf(XX,YY,ZZ,level,'showtext','off','linestyle','-');
+colormap jet; caxis([100 350]); colorbar;
+%clabel(C,h,'FontSize',8,'FontSmoothing','on');
+title(sprintf('%.4f AU', dau));
+set(gcf, 'Position', [819 189 425 373]);
 
-imgname    = sprintf('images/pelivan_d%.1f_g%.0f.png', dau, gamma);
+imgname    = sprintf('images/hourangle_d%.4f.png', dau);
 % print(gcf,imgname,'-dpng','-r600');
+
+% close all;
+% hold on; grid on; axis equal;
+% scatter3(m(1,:), m(2,:), m(3,:), 'k')
+% ru = r/my_norm(r)*100;
+% scatter3(ru(1,:), ru(2,:), ru(3,:), 'r', 'filled');
